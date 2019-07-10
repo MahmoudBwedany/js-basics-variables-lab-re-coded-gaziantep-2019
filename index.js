d65a40f93bea4ab61cf5be910c42644dba14a3c9
Mahmoud Bwedany <mahmoudbwedany@gmail.com>
2019-07-10 05:29:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyNmae = "Scuber";
-let mostProfitableNeighborhood = "Chelsea"
-let companyCeo = "Susan Smith"
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
